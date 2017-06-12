21c22ab4a6768aa0dc74d14fc4285b01b88671c6
Anselmo <chemo@Anselmos-iMac.local>
2017-06-12 12:28:04 -0500

    Operacion compleja category

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,17 @@ Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' =>['ind
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' =>['index']]);
 Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' =>['index']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' =>['index']]);
+
 Route::resource('categories', 'Category\CategoryController', ['except' =>['create', 'edit']]);
+Route::resource('categories.products', 'Category\CategoryProductController', ['only' =>['index']]);
+
 Route::resource('products', 'Product\ProductController', ['only' =>['index', 'show']]);
+
 Route::resource('transactions', 'Transaction\TransactionController', ['only' =>['index', 'show']]);
 Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' =>['index']]);
 Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' =>['index']]);
+
 Route::resource('sellers', 'Seller\SellerController', ['only' =>['index', 'show']]);
+
 Route::resource('users', 'User\UserController', ['except' =>['create', 'edit']]);
 
